e01b3bcccaae1d6b6732255da3b26452d1229b03
ssbtt114514 <ssbtt114514@gmail.com>
2026-07-18 18:32:31 +0800

    fix: 修复非官方版本提示弹窗点击关闭按钮无反应
    原因：showUnofficialVersionWarning 是普通 var Boolean，不是 Compose State，
    修改值不会触发重组，弹窗无法关闭。
    修复：改用 mutableStateOf(true) 使其成为可观察的 Compose 状态。

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/activities/MainActivity.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/activities/MainActivity.kt
@@ -32,6 +32,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.Lifecycle
@@ -173,7 +175,7 @@ class MainActivity : BaseAppCompatActivity() {
      * 是否开启捕获按键模式
      */
     private var isCaptureKey = false
-    private var showUnofficialVersionWarning = true
+    private var showUnofficialVersionWarning by mutableStateOf(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
